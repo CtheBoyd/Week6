6d9f9f89a6876e7aab44f70a815ff19ff0b65377
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-05-04 19:26:50 -0700

    update

--- add_surname.py
+++ add_surname.py
@@ -4,10 +4,10 @@
 # Description: Takes a list of names, separates names that start with "K" and then adds a surname to each.
 #
 
-def add_surname():
+def add_surname(fore):
     """Takes a list of names, separates names that start with "K" and then adds a surname to each."""
-    k_fore = [i + " " + surname for i in fore if i[0] == "K"]
-    return k_fore
+    list = [name + 'Kardashian' for name in fore if name[0] == "K"]
+    return list
 
 #fore = ["Kiki", "Krystal", "Pavel", "Marykay", "Annie", "Koala"]
 #surname = "Kardashian"
